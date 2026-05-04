1ca9716c9a5abb7a1678834dfc59deeb5d2619ab
Matheus OLiveira <ferraiolimatheus@gmail.com>
2026-05-04 00:10:35 -0300

    mais uma correcao pro deploy, agr vai

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/adminController');
-const { apenasAdmin } = require('../../middlewares/authMiddleware');
+const { apenasAdmin } = require('../middlewares/authMiddleware');
 
 router.get('/admin/pedidos', apenasAdmin, controller.getTodosPedidos);
 router.put('/admin/pedidos/:id/status', apenasAdmin, controller.atualizarStatus);
